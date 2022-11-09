0dc4b2b65968fbd4ed38a71c6cabb6e0fde38dad
ChenQirui1 <ivanchen.code@gmail.com>
2022-11-09 09:02:54 +0000

    lr wip

--- supervised-learning/linear_regression.jl
+++ supervised-learning/linear_regression.jl
@@ -1,2 +1,19 @@
+using Pkg
+Pkg.add("CSV")
+Pkg.add("DataFrames")
+using CSV
+using DataFrames
+
+df = CSV.read("../dataset/Iris.csv", DataFrame)
+
+X = df[:,2:4]
+
 var = [[1,2,3] [4,5,6]]
-var
+println(var)
+
+#with open(""../Dataset/Iris.csv")
+
+h(x,θ) = θ
+
+println(X)
+
