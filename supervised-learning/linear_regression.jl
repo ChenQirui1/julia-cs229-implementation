21e8a2451229f96d31ce107538877b79c8fc9388
ChenQirui1 <ivanchen.code@gmail.com>
2022-11-27 19:28:08 +0800

    updated linear regression

--- supervised-learning/linear_regression.jl
+++ supervised-learning/linear_regression.jl
@@ -6,14 +6,35 @@ using DataFrames
 
 df = CSV.read("../dataset/Iris.csv", DataFrame)
 
-X = df[:,2:4]
+#X = df[:,2:4] #design matrix 
+X = hcat(ones(150),Matrix(df[:,2:4])) #design matrix
 
 var = [[1,2,3] [4,5,6]]
 println(var)
 
 #with open(""../Dataset/Iris.csv")
 
-h(x,θ) = θ
+#hypothesis function
+h(x,θ) = θ'x
 
-println(X)
+#cost function J
+J(θ) = (0.5)(Xθ-y)'(Xθ-y)
+
+#\
+
+function BatchDescendingFunc(x)
+
+
+    end;
+
+function BatchGradientDescent(X,y,iteration,learning_rate)
+    α = learning_rate
+    θ = ones(size(X, 2))
+    
+    for epoch in epochs
+        θj = X[:]
+        idxθj = 
+        #result = map(θ -> 
+    
+
 
