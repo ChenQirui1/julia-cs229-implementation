16cbbe38ba3804da884c7b942e7e22b788861fda
ChenQirui1 <ivanchen.code@gmail.com>
2022-12-11 22:53:23 +0800

    Amended Regression

--- supervised-learning/linear_regression.jl
+++ supervised-learning/linear_regression.jl
@@ -1,40 +1,47 @@
 using Pkg
 Pkg.add("CSV")
 Pkg.add("DataFrames")
+Pkg.add("ForwardDiff")
 using CSV
 using DataFrames
+using ForwardDiff
+using Random
 
-df = CSV.read("../dataset/Iris.csv", DataFrame)
+df = CSV.read("..//dataset//Iris.csv", DataFrame)
 
-#X = df[:,2:4] #design matrix 
+#Declaring global varib
 X = hcat(ones(150),Matrix(df[:,2:4])) #design matrix
-
-var = [[1,2,3] [4,5,6]]
-println(var)
-
-#with open(""../Dataset/Iris.csv")
+θ = Vector{Int}(undef,size(df)[2])
+rand!(θ,1:100)
+y = df[:,5]
 
 #hypothesis function
-h(x,θ) = θ'x
+h(x) = θ'x
 
 #cost function J
-J(θ) = (0.5)(Xθ-y)'(Xθ-y)
-
-#\
-
-function BatchDescendingFunc(x)
+J(θ) = (0.5)(X*θ-y)'(X*θ-y)
 
 
+function LMSUpdateRule(xi,yi,α,θj)
+    θj .+= α*(yi - h(xi))*xi[j]
     end;
 
-function BatchGradientDescent(X,y,iteration,learning_rate)
-    α = learning_rate
-    θ = ones(size(X, 2))
+function BatchGradientDescent(X,y,α)
     
-    for epoch in epochs
-        θj = X[:]
-        idxθj = 
-        #result = map(θ -> 
-    
+        for (idxθj,θj) in enumerate(θ)
+            for (idx, xi) in enumerate(X)
+                LMSUpdateRule(xi, y[idx], α, θj)
+            end;
+            θ[idxθj] = θj
+
+        end;
+
+    return θ
+    end;
 
 
+
+
+BatchGradientDescent(X,y,0.1)
+
+println(J(θ))
